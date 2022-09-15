306ceb29c8ae5f46b4e4d47d9b29041aa36908e2
Matiasballesta <75946070+Matiasballesta@users.noreply.github.com>
2022-09-14 22:39:51 -0300

    Primer commit

---
+++ src/components/ListadoPacientes.jsx
@@ -0,0 +1,35 @@
+import Paciente from "./Paciente";
+
+const ListadoPacientes = ({ pacientes, setPaciente, Eliminarpaciente }) => {
+
+  return (
+    <div className="md:w-1/2 lg:w-3/5 ml-5 md:h-screen overflow-y-scroll">
+      {pacientes && pacientes.length ? (
+        <>
+          <h2 className="font-black text-3xl text-center">Listado Pacientes</h2>
+          <p className="text-xl mt-5 mb-10 text-center">
+            Administra tus {""}
+            <span className="text-indigo-600 font-bold">Pacientes y Citas</span>
+          </p>
+
+          {pacientes.map((paciente) => (
+            <Paciente key={paciente.id}
+             paciente={paciente}
+             setPaciente={setPaciente} 
+             Eliminarpaciente={Eliminarpaciente}/>
+          ))}
+        </>
+      ) : (
+        <>
+        <h2 className="font-black text-3xl text-center">No hay pacientes</h2>
+          <p className="text-xl mt-5 mb-10 text-center">
+            Comienza agregando pacientes {""}
+            <span className="text-indigo-600 font-bold">Y apareceran aqui</span>
+          </p>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default ListadoPacientes;
---
+++ src/components/Paciente.jsx
@@ -0,0 +1,58 @@
+import { useEffect } from "react";
+
+const Paciente = ({paciente, setPaciente, Eliminarpaciente}) => {
+
+  const handleEliminar = () => {
+    const respuesta = confirm('Deseas eliminar este paciente?');
+
+    if(respuesta){
+      Eliminarpaciente(paciente.id)
+    }
+  }
+
+  return (
+    <div className="mx-5 my-10 bg-white shadow-md px-5 py-10 rounded-xl">
+      <p className="font-bold mb-3 text-gray-700 uppercase">
+        Nombre: {""}
+        <span className="font-normal normal-case">{paciente.nombre}</span>
+      </p>
+      <p className="font-bold mb-3 text-gray-700 uppercase">
+        Propietario: {""}
+        <span className="font-normal normal-case">{paciente.propietario}</span>
+      </p>
+      <p className="font-bold mb-3 text-gray-700 uppercase">
+        Email: {""}
+        <span className="font-normal normal-case">
+          {paciente.email}
+        </span>
+      </p>
+      <p className="font-bold mb-3 text-gray-700 uppercase">
+        Fecha Alta: {""}
+        <span className="font-normal normal-case">{paciente.fecha}</span>
+      </p>
+      <p className="font-bold mb-3 text-gray-700 uppercase">
+        Sintomas: {""}
+        <span className="font-normal normal-case">
+          {paciente.sintomas}
+        </span>
+      </p>
+      <div className="flex justify-between mt-10">
+        <button
+        onClick={() => setPaciente(paciente)}
+        type="button"
+        className="py-2 px-10 bg-indigo-600 hover:bg-indigo-700 text-white font-bold uppercase rounded">
+          Editar
+        </button>
+        <button
+        type="button"
+        className="py-2 px-10 bg-red-600 hover:bg-red-700 text-white font-bold uppercase rounded"
+        onClick={handleEliminar}>
+          Eliminar
+        </button>
+      </div>
+
+    </div>
+  );
+};
+
+export default Paciente;
